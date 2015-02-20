b59c5ff3bdc4ef56e5f8163c7cedd7cd553e44cc
Krzysztof Grochocki <beherit@users.noreply.github.com>
2015-02-20 12:21:43 +0100

    * Przepisanie programu pod kompilator Embarcadero C++ Builder XE3. * Pe?ne sk?rkowanie okna programu wykorzystuj?c styl AQQ. * Aktualizacja komponentu ZipForge. * Zmiana ikonki programu. * Rezygnacja z w?tk?w podczas tworzenia/przywracania kopii zapasowej profilu. * Poprawa dzia?ania programu uruchamianego z parametrem -backup. * Zapisywanie w rejestrze ?cie?ki, gdzie maj? by? zapisywane kopie zapasowe. * Zablokowanie mo?liwo?ci zamkni?cia aplikacji podczas tworzenia/przywracania kopii zapasowej. * Inne mniej wa?ne poprawki optymalizacyjne oraz kosmetyczne.

--- BackupAQQFrm.h
+++ BackupAQQFrm.h
@@ -2,104 +2,86 @@
 #ifndef BackupAQQFrmH
 #define BackupAQQFrmH
 //---------------------------------------------------------------------------
-#include <Classes.hpp>
-#include <Controls.hpp>
-#include <StdCtrls.hpp>
-#include <Forms.hpp>
-#include <ActnList.hpp>
-#include <ComCtrls.hpp>
-#include <Dialogs.hpp>
-#include <ExtCtrls.hpp>
-#include <FileCtrl.hpp>
-#include "IdBaseComponent.hpp"
-#include "IdThreadComponent.hpp"
-#include "LMDBaseEdit.hpp"
-#include "LMDBrowseEdit.hpp"
-#include "LMDControl.hpp"
-#include "LMDCustomBevelPanel.hpp"
-#include "LMDCustomBrowseEdit.hpp"
-#include "LMDCustomControl.hpp"
-#include "LMDCustomEdit.hpp"
-#include "LMDCustomPanel.hpp"
-#include "LMDPNGImage.hpp"
-#include <ImgList.hpp>
-#include "LMDCustomComponent.hpp"
-#include "LMDWndProcComponent.hpp"
-#include "LMDTrayIcon.hpp"
+#include "acPNG.hpp"
+#include "acProgressBar.hpp"
+#include "sBevel.hpp"
+#include "sButton.hpp"
+#include "sCustomComboEdit.hpp"
+#include "sDialogs.hpp"
+#include "sLabel.hpp"
+#include "sListBox.hpp"
+#include "sMaskEdit.hpp"
+#include "sRadioButton.hpp"
+#include "sSkinManager.hpp"
+#include "sTooledit.hpp"
 #include "ZipForge.hpp"
+#include <System.Actions.hpp>
+#include <System.Classes.hpp>
+#include <Vcl.ActnList.hpp>
+#include <Vcl.ComCtrls.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.Dialogs.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.FileCtrl.hpp>
+#include <Vcl.Mask.hpp>
+#include <Vcl.StdCtrls.hpp>
 //---------------------------------------------------------------------------
 class TBackupAQQForm : public TForm
 {
 __published:	// IDE-managed Components
-	TTabControl *BackupsListTabControl;
-	TLabel *BackupsListLabel;
-	TImage *Image3;
-	TFileListBox *BackupsFileListBox;
-	TButton *BrowseButton;
-	TTabControl *RestoreTabControl;
-	TLabel *RestoreLabel;
-	TImage *Image4;
-	TProgressBar *ProgressBar2;
-	TTabControl *BackupTabControl;
-	TLabel *BackupLabel;
-	TImage *Image2;
-	TProgressBar *ProgressBar;
-	TTabControl *MainTabControl;
-	TLabel *ProfilesListLabel;
-	TLabel *SaveToPathLabel;
-	TImage *Image1;
-	TListBox *ProfilesListBox;
-	TRadioButton *CreateBackupRadioButton;
-	TRadioButton *RestoreProfileRadioButton;
-	TLMDBrowseEdit *BrowseEdit;
-	TButton *ExitButton;
-	TButton *NextButton;
-	TButton *PreviousButton;
-	TActionList *ActionList;
-	TAction *aGetProfilesList;
-	TAction *aDoBackup;
-	TAction *aRestoreBackup;
-	TOpenDialog *OpenDialog;
-	TIdThreadComponent *BackupIdThreadComponent;
-	TIdThreadComponent *RestoreIdThreadComponent;
-	TIdThreadComponent *ManualRestoreIdThreadComponent;
-	TAction *aCommandDoBackup;
-	TLMDTrayIcon *TrayIcon;
-	TBevel *MainBevel;
+	TsBevel *Bevel;
+	TsSkinManager *sSkinManager;
+	TPageControl *PageControl;
+	TTabSheet *WizzardTabSheet;
 	TZipForge *ZipForge;
-	TLabel *ProgressLabel;
-	TLabel *ProgressLabel2;
-	TTimer *Win7ProgressTimer;
-	TZipForge *ZipForgeCommand;
-	void __fastcall aGetProfilesListExecute(TObject *Sender);
+	TsButton *CloseButton;
+	TsButton *NextButton;
+	TsButton *PreviousButton;
+	TActionList *ActionList;
+	TAction *aExit;
+	TsListBox *ProfilesListBox;
+	TAction *aGetProfiles;
+	TsRadioButton *CreateBackupRadioButton;
+	TsRadioButton *RestoreProfileRadioButton;
+	TsDirectoryEdit *sDirectoryEdit;
+	TImage *Image1;
+	TTrayIcon *TrayIcon;
+	TAction *aBackupProfile;
+	TAction *aCommandBackupProfile;
+	TTabSheet *ProgressTabSheet;
+	TsLabel *InfoLabel;
+	TsProgressBar *ProgressBar;
+	TsLabel *ProgressLabel;
+	TImage *Image2;
+	TTabSheet *BackupsListTabSheet;
+	TImage *Image3;
+	TsLabel *BackupsListLabel;
+	TsButton *BrowseButton;
+	TFileListBox *BackupsFileListBox;
+	TsOpenDialog *sOpenDialog;
+	void __fastcall aExitExecute(TObject *Sender);
+	void __fastcall aGetProfilesExecute(TObject *Sender);
 	void __fastcall FormShow(TObject *Sender);
-	void __fastcall ExitButtonClick(TObject *Sender);
-	void __fastcall aDoBackupExecute(TObject *Sender);
 	void __fastcall NextButtonClick(TObject *Sender);
 	void __fastcall PreviousButtonClick(TObject *Sender);
-	void __fastcall aRestoreBackupExecute(TObject *Sender);
-	void __fastcall BrowseButtonClick(TObject *Sender);
-	void __fastcall OpenDialogCanClose(TObject *Sender, bool &CanClose);
-	void __fastcall BackupIdThreadComponentRun(TIdThreadComponent *Sender);
-	void __fastcall RestoreIdThreadComponentRun(TIdThreadComponent *Sender);
-	void __fastcall ManualRestoreIdThreadComponentRun(TIdThreadComponent *Sender);
-	void __fastcall aCommandDoBackupExecute(TObject *Sender);
-	void __fastcall FormPaint(TObject *Sender);
+	void __fastcall aBackupProfileExecute(TObject *Sender);
+	void __fastcall aCommandBackupProfileExecute(TObject *Sender);
 	void __fastcall ZipForgeFileProgress(TObject *Sender, UnicodeString FileName, double Progress,
-          TZFProcessOperation Operation, TZFProgressPhase ProgressPhase, bool &Cancel);
+		  TZFProcessOperation Operation, TZFProgressPhase ProgressPhase, bool &Cancel);
 	void __fastcall ZipForgeOverallProgress(TObject *Sender, double Progress, TZFProcessOperation Operation,
           TZFProgressPhase ProgressPhase, bool &Cancel);
-	void __fastcall CreateBackupRadioButtonClick(TObject *Sender);
-	void __fastcall RestoreProfileRadioButtonClick(TObject *Sender);
-	void __fastcall Win7ProgressTimerTimer(TObject *Sender);
-	void __fastcall ZipForgeCommandOverallProgress(TObject *Sender, double Progress,
-          TZFProcessOperation Operation, TZFProgressPhase ProgressPhase, bool &Cancel);
-
-
-
-
+	void __fastcall BrowseButtonClick(TObject *Sender);
+	void __fastcall sOpenDialogCanClose(TObject *Sender, bool &CanClose);
+	void __fastcall WizzardTabSheetShow(TObject *Sender);
+	void __fastcall sDirectoryEditAfterDialog(TObject *Sender, UnicodeString &Name,
+          bool &Action);
+	void __fastcall ProfilesListBoxClick(TObject *Sender);
+	void __fastcall sDirectoryEditChange(TObject *Sender);
+	void __fastcall FormCloseQuery(TObject *Sender, bool &CanClose);
 private:	// User declarations
-	void __fastcall FindDir(TListBox *lista, UnicodeString Dir);
+	void __fastcall FindDirectories(TListBox* ListBox, UnicodeString Path);
+	void __fastcall DeleteFiles(UnicodeString Path);
+	void __fastcall aRestoreProfile(UnicodeString FileName);
 public:		// User declarations
 	__fastcall TBackupAQQForm(TComponent* Owner);
 };
