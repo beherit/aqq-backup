18761959d07c8aa76669f5382d1a37437bd78e19
Krzysztof Grochocki <beherit@users.noreply.github.com>
2015-02-20 12:15:53 +0100

    * Przepisanie programu pod kompilator Embarcadero C++ Builder 2010. * Poprawki w kodzie. * Informacja o wy??czeniu AQQ przy przywracaniu/tworzeniu kopii profilu AQQ.

--- BackupAQQFrm.h
+++ BackupAQQFrm.h
@@ -1,5 +1,4 @@
 //---------------------------------------------------------------------------
-
 #ifndef BackupAQQFrmH
 #define BackupAQQFrmH
 //---------------------------------------------------------------------------
@@ -7,12 +6,13 @@
 #include <Controls.hpp>
 #include <StdCtrls.hpp>
 #include <Forms.hpp>
-#include "XPMan.hpp"
-#include <ComCtrls.hpp>
-#include <TabNotBk.hpp>
-#include <ExtCtrls.hpp>
 #include <ActnList.hpp>
+#include <ComCtrls.hpp>
+#include <Dialogs.hpp>
+#include <ExtCtrls.hpp>
 #include <FileCtrl.hpp>
+#include "IdBaseComponent.hpp"
+#include "IdThreadComponent.hpp"
 #include "LMDBaseEdit.hpp"
 #include "LMDBrowseEdit.hpp"
 #include "LMDControl.hpp"
@@ -22,76 +22,67 @@
 #include "LMDCustomEdit.hpp"
 #include "LMDCustomPanel.hpp"
 #include "LMDPNGImage.hpp"
-#include <Dialogs.hpp>
-#include "IdThreadComponent.hpp"
-#include <IdBaseComponent.hpp>
 //---------------------------------------------------------------------------
 class TBackupAQQForm : public TForm
 {
 __published:	// IDE-managed Components
-        TXPMan *XPMan;
-        TTabControl *MainTabControl;
-        TTabControl *BackupTabControl;
-        TListBox *ProfilesListBox;
-        TRadioButton *CreateBackupRadioButton;
-        TRadioButton *RestoreProfileRadioButton;
-        TButton *ExitButton;
-        TButton *NextButton;
-        TButton *PreviousButton;
-        TActionList *ActionList;
-        TAction *aGetProfilesList;
-        TAction *aDoBackup;
-        TLabel *ProfilesListLabel;
-        TLabel *MainInfoLabel;
-        TLabel *BackupLabel;
-        TTabControl *BackupsListTabControl;
-        TLabel *BackupsListLabel;
-        TFileListBox *BackupsFileListBox;
-        TAction *aRestoreBackup;
-        TTabControl *RestoreTabControl;
-        TLabel *RestoreLabel;
-        TLabel *SaveToPathLabel;
-        TLMDBrowseEdit *BrowseEdit;
-        TButton *BrowseButton;
-        TImage *Image1;
-        TImage *Image2;
-        TImage *Image4;
-        TImage *Image3;
-        TOpenDialog *OpenDialog;
-        TIdThreadComponent *BackupIdThreadComponent;
-        TIdThreadComponent *RestoreIdThreadComponent;
-        TIdThreadComponent *ManualRestoreIdThreadComponent;
-        TTimer *GetProgressTimer;
-        TProgressBar *ProgressBar;
-        TProgressBar *ProgressBar2;
-        TCheckBox *PluginsCheckBox;
-        TCheckBox *ThemesCheckBox;
-        TCheckBox *SmileysCheckBox;
-        TCheckBox *CustomEmotsCheckBox;
-        TCheckBox *IncomingCheckBox;
-        TCheckBox *TempCheckBox;
-        TLabel *Label1;
-        void __fastcall aGetProfilesListExecute(TObject *Sender);
-        void __fastcall FormShow(TObject *Sender);
-        void __fastcall ExitButtonClick(TObject *Sender);
-        void __fastcall aDoBackupExecute(TObject *Sender);
-        void __fastcall NextButtonClick(TObject *Sender);
-        void __fastcall PreviousButtonClick(TObject *Sender);
-        void __fastcall aRestoreBackupExecute(TObject *Sender);
-        void __fastcall BrowseButtonClick(TObject *Sender);
-        void __fastcall OpenDialogCanClose(TObject *Sender,
-          bool &CanClose);
-        void __fastcall BackupIdThreadComponentRun(
-          TIdCustomThreadComponent *Sender);
-        void __fastcall RestoreIdThreadComponentRun(
-          TIdCustomThreadComponent *Sender);
-        void __fastcall ManualRestoreIdThreadComponentRun(
-          TIdCustomThreadComponent *Sender);
-        void __fastcall GetProgressTimerTimer(TObject *Sender);
+	TTabControl *BackupsListTabControl;
+	TLabel *BackupsListLabel;
+	TImage *Image3;
+	TFileListBox *BackupsFileListBox;
+	TButton *BrowseButton;
+	TTabControl *RestoreTabControl;
+	TLabel *RestoreLabel;
+	TImage *Image4;
+	TProgressBar *ProgressBar2;
+	TTabControl *BackupTabControl;
+	TLabel *BackupLabel;
+	TImage *Image2;
+	TProgressBar *ProgressBar;
+	TTabControl *MainTabControl;
+	TLabel *ProfilesListLabel;
+	TLabel *SaveToPathLabel;
+	TLabel *ExecuteLabel;
+	TImage *Image1;
+	TListBox *ProfilesListBox;
+	TRadioButton *CreateBackupRadioButton;
+	TRadioButton *RestoreProfileRadioButton;
+	TLMDBrowseEdit *BrowseEdit;
+	TCheckBox *PluginsCheckBox;
+	TCheckBox *ThemesCheckBox;
+	TCheckBox *SmileysCheckBox;
+	TCheckBox *CustomEmotsCheckBox;
+	TCheckBox *IncomingCheckBox;
+	TCheckBox *TempCheckBox;
+	TButton *ExitButton;
+	TButton *NextButton;
+	TButton *PreviousButton;
+	TActionList *ActionList;
+	TAction *aGetProfilesList;
+	TAction *aDoBackup;
+	TAction *aRestoreBackup;
+	TOpenDialog *OpenDialog;
+	TIdThreadComponent *BackupIdThreadComponent;
+	TIdThreadComponent *RestoreIdThreadComponent;
+	TIdThreadComponent *ManualRestoreIdThreadComponent;
+	TIdThreadComponent *GetProgressIdThreadComponent;
+	void __fastcall aGetProfilesListExecute(TObject *Sender);
+	void __fastcall FormShow(TObject *Sender);
+	void __fastcall ExitButtonClick(TObject *Sender);
+	void __fastcall aDoBackupExecute(TObject *Sender);
+	void __fastcall NextButtonClick(TObject *Sender);
+	void __fastcall PreviousButtonClick(TObject *Sender);
+	void __fastcall aRestoreBackupExecute(TObject *Sender);
+	void __fastcall BrowseButtonClick(TObject *Sender);
+	void __fastcall OpenDialogCanClose(TObject *Sender, bool &CanClose);
+	void __fastcall BackupIdThreadComponentRun(TIdThreadComponent *Sender);
+	void __fastcall RestoreIdThreadComponentRun(TIdThreadComponent *Sender);
+	void __fastcall ManualRestoreIdThreadComponentRun(TIdThreadComponent *Sender);
+	void __fastcall GetProgressIdThreadComponentRun(TIdThreadComponent *Sender);
 private:	// User declarations
-        void __fastcall FindDir(TListBox *lista, AnsiString Dir);
+	void __fastcall FindDir(TListBox *lista, AnsiString Dir);
 public:		// User declarations
-        __fastcall TBackupAQQForm(TComponent* Owner);
+	__fastcall TBackupAQQForm(TComponent* Owner);
 };
 //---------------------------------------------------------------------------
 extern PACKAGE TBackupAQQForm *BackupAQQForm;
